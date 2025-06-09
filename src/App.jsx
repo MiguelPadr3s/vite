71977ffcbf1e612e0e0068d24e2cb0f8e9500b11
mikeproxd1 <A01252852@itesm.mx>
2025-06-09 16:58:49 -0700

    Cambios con alien

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useLoader } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Suspense } from 'react';
+
+{/* carga el modelo */}
+function Model() {
+  const result = useLoader(GLTFLoader, '/Alien/scene.gltf');
+  result.scene.position.set(0, 2.8, 1);
+  result.scene.scale.set(1, 1, 1);
+  result.scene.rotation.y = Math.PI;
+  return <primitive object={result.scene} />;
+}
 
 function App() {
-  return ( 
-  <div id='canvas-container'>
-    <Canvas /*camera={{ position: [2, 2, 2] }}*/>
-      <mesh>
+  return (
+    <div id='canvas-container'>
+      {/* Luz y posicion de camara */}
+      <Canvas camera={{ position: [20, 14, 0], fov: 30 }}>
+        <ambientLight intensity={0.8} />
+        <directionalLight position={[5, 10, 5]}
+          intensity={15}/>
         <OrbitControls />
-       {/* <sphereGeometry args={[ 3, 80, 80]}/> */}
-       {/* <boxGeometry args={[ 2, 3, 2]}/> */}
-       <torusKnotGeometry args={[ 1.7, 0.3, 256, 256]} />
-        <meshToonMaterial color={0x00bfff} />
-        <directionalLight position={[2, 5, 1]}/>
-      </mesh>
-    </Canvas>
-  </div>
 
+        {/* codigo del suelo */}
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow={false}>
+          <planeGeometry args={[20, 20]} />
+          <meshBasicMaterial color="#e49292" />
+          <gridHelper args={[20, 20]} rotation={[-Math.PI / 2, 0, 0]} />
+        </mesh>
+
+        {/* codigo del modelo */}
+        <Suspense fallback={null}>
+          <Model/>
+        </Suspense>
+      </Canvas>
+    </div>
   );
 }
 
-export default App
+export default App;
