357da8e01cf9c4af1d1dd7bb8660064c2bd761e5
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-06-10 14:19:21 -0700

    Cambio de modelo y posicion

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import { Color} from 'three';
 
 
 {/* carga el modelo */}
-function Model() {
+/*function Model() {
   const result = useLoader(GLTFLoader, '/Alien/scene.gltf');
   result.scene.rotation.y = Math.PI;
   return <primitive 
   object={result.scene} 
   position={[0, 2.8, 1]} 
   scale={[1, 1, 1]}/>;
-}
+}*/
 function Model2() {
   const result = useLoader(GLTFLoader, '/gabinete/scene.gltf');
   result.scene.rotation.y = Math.PI;
@@ -28,22 +28,23 @@ function Model2() {
       />
  
       {/* Anotacion 1 */}
-<Html position={[5.01, -0.8, -5]} center>
-  <div className="annotation" onClick={() => window.location.href = '/puerta'}>
+<Html position={[-7.14, 0.5, -0.67]} center>
+  <div className="annotation" onClick={() => window.location.href = 'https://192.168.30.252:4443/#%2FEnterprise%20Server%2FBMS%2FProyectos%2FFord%2FSubestaci%C3%B3n%2FEnerg%C3%ADa'}>
     Unifilares
   </div>
 </Html>
 
+
 {/* Anotacion 2 */}
-<Html position={[5.02, -0.7, -5.1]} center>
-  <div className="annotation" onClick={() => window.location.href = '/panel'}>
+<Html position={[3.2, 0.8, 0.8]} center>
+  <div className="annotation" onClick={() => window.location.href = 'https://192.168.30.252:4443/#%2FEnterprise%20Server%2FBMS%2FProyectos%2FFord%2FSubestaci%C3%B3n%2FCuarto%203D%2FAcometida%20Sur'}>
     Acometida Sur
   </div>
 </Html>
 
 {/* Anotacion 3 */}
-<Html position={[5.00, -0.6, -4.9]} center>
-  <div className="annotation" onClick={() => window.location.href = '/estado'}>
+<Html position={[3.2, 0.8, -2.1]} center>
+  <div className="annotation" onClick={() => window.location.href = 'https://192.168.30.252:4443/#%2FEnterprise%20Server%2FBMS%2FProyectos%2FFord%2FSubestaci%C3%B3n%2FCuarto%203D%2FAcometida%20norte%202'}>
     Acometida Norte
   </div>
 </Html>
@@ -54,7 +55,7 @@ function Model2() {
 function UpdateSceneBackground() {
   const { scene } = useThree();
 
-  const texture = useTexture('/cave.jpg');
+  const texture = useTexture('/office.jpg');
 
   scene.background = texture;
 
@@ -65,7 +66,7 @@ function App() {
   return (
     <div id='canvas-container'>
       {/* Luz y posicion de camara */}
-      <Canvas camera={{ position: [20, 14, 0], fov: 30 }}>
+      <Canvas camera={{ position: [30, 14, -15], fov: 15 }}>
         
         <ambientLight intensity={0.8} />
         <directionalLight position={[5, 10, 5]}
@@ -75,14 +76,14 @@ function App() {
         {/* codigo del suelo */}
         <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow={false}>
           <planeGeometry args={[20, 20]} />
-          <meshBasicMaterial color="#e49292" />
+          <meshBasicMaterial color="#a5a4a3" />
           <gridHelper args={[20, 20]} rotation={[-Math.PI / 2, 0, 0]} />
         </mesh>
 
         {/* codigo del modelo */}
         <Suspense fallback={null}>
           <UpdateSceneBackground/>
-          <Model/>
+         {/* <Model/> */}
           <Model2/>
         </Suspense>
       </Canvas>
